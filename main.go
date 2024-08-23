3cdea3118977a27078942c4476eef3e2ace570c9
samar <scipioresearch2020@gmail.com>
2024-08-23 18:33:04 +0530

    fxbstorage h changed

--- main.go
+++ main.go
@@ -9,27 +9,31 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
+var basedir string
+
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		panic(err)
 	}
-	basedir := os.Getenv("BASEDIR")
+	basedir = os.Getenv("BASEDIR")
 	hostname := os.Getenv("HOSTNAME")
 	if err := os.Setenv("HOSTNAME", hostname); err != nil {
 		panic(err)
 	}
 	e := echo.New()
 	e.Use(middleware.Logger())
-	e.GET("/:baseFolder/:subFolder/:fileName", func(c echo.Context) error {
-		finalPath := basedir + c.Param("baseFolder") + "/" + c.Param("subFolder") + "/" + c.Param("fileName")
-		file, err := os.Open(finalPath)
-		imageFormat := strings.Split(file.Name(), ".")[len(strings.Split(file.Name(), "."))-1]
-		if err != nil {
-			return err
-		}
-		defer file.Close()
-		return c.Stream(200, "image/"+imageFormat, file)
-	})
+	e.GET("fxb-storage/:baseFolder/:subFolder/:fileName", GetFXBStorage)
 	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func GetFXBStorage(c echo.Context) error {
+	finalPath := basedir + c.Param("baseFolder") + "/" + c.Param("subFolder") + "/" + c.Param("fileName")
+	file, err := os.Open(finalPath)
+	imageFormat := strings.Split(file.Name(), ".")[len(strings.Split(file.Name(), "."))-1]
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	return c.Stream(200, "image/"+imageFormat, file)
 }
